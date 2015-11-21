8c444d8479bfef0b948bb9b974567af392d2600c
Sagun B <sagun.b@gmail.com>
2015-11-21 11:26:18 -0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn webserver.webhost:app --log-file
---
+++ app.py
@@ -0,0 +1,23 @@
+import twilio.twiml
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+SECRET_KEY = 'development key'
+app.secret_key = SECRET_KEY
+
+
+###
+# Routing for your application.
+###
+
+@app.route('/')
+def receive_sms():
+    """Respond to a new sms"""
+
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
